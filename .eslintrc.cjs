bfeb8cb78eb127637ac172d62e5746aa1d59da35
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-01-26 16:38:11 +0600

    Add header component and remove unused files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,21 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  overrides: [
+    {
+      files: ['src/views/**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 0
+      }
+    }
+  ],
+  rules: { 'vue/multi-word-component-names': 0 }
 }
